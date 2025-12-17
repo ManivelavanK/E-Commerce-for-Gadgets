7b347c170884f9daecbdd02bba98a538d3e6819e
ManivelavanK <manivelavan420534@gmail.com>
2025-12-17 10:14:13 +0530

    Delete Backend directory

--- Backend/Routers/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createContact, getContacts } = require('../Controllers/contactController');
-
-const router = express.Router();
-
-router.post('/', createContact);
-router.get('/', getContacts);
-
-module.exports = router;
